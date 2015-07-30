7d42daedee88ea3c51ae01bb58d12baa5abccbac
Ben Preston <brp22@cornell.edu>
2015-07-30 17:58:47 -0400

    mas changes

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'jbuilder', '~> 2.0'
 
 gem 'bootstrap-sass'
 
+gem 'devise'
+
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'https://young-stream-9245.herokuapp.com' }
 end
